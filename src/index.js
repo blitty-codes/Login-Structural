5ff2f9855e3df235446b80e9018b3e92a8adec11
blitty <blittynet@gmail.com>
2020-07-25 17:04:33 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import './assets/css/index.css'
 import * as serviceWorker from './serviceWorker';
 
+/*
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
+
+import Routes from "./Routes";
+
+render(<Routes />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
